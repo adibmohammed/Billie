4802958dfac45a674a42139b96d43f67620237c4
Mohammed Adib <adibmohammed95@gmail.com>
2021-03-07 18:17:57 +0100

    Modles, Helpers, Seeding

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,27 +1,32 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+	username: String,
+	email: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	profile: {
+		type: String,
+		enum: [ 'Family', 'Business', 'Student', 'Standard' ]
+	},
+	gender: {
+		type: String,
+		enum: [ 'Woman', 'Man', 'Prefer Not To' ]
+	},
+	avatar: {
+		type: String,
+		default: 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'
+	},
+	myexpense: { type: Schema.Types.ObjectId, ref: 'expenses' },
+	myincome: { type: Schema.Types.ObjectId, ref: 'income' }
+});
 
- username: String,
- email: {
-     type: String,
-     unique: true,
-     required: true
- },
- password: {
-     type: String,
-     required: true,
- },
- profile: {
-     type: String,
-     enum : ["Family", "Business", "Student", "Standard"]
- },
- gender: {
-     type: String,
-     enum: ["Woman", "Man", "Prefer Not To"]
- }
-})
-
-const UserModel = mongoose.model("user", userSchema)
+const UserModel = mongoose.model('user', userSchema);
 module.exports = UserModel;

f007063f4fa33e99b0c755881fae7c21569109f6
Anna Lefour <anna.lefour@laposte.net>
2021-03-08 15:43:22 +0100

    Create income okayish (but pb with enum) and all incomes done

--- routes/IncomesCRUD/createIncome.js
+++ routes/IncomesCRUD/createIncome.js
@@ -2,17 +2,15 @@ const express = require("express");
 const router = express.Router();
 const IncomeModel = require("../../models/IncomeModel");
 
-
 //GET route to enter a new income
 router.get("/income/new", (req, res, next) => {
   res.render("incomes/createIncome");
 });
 
-
 //POST route to send the new income via a form
 router.post("/income/new", async (req, res, next) => {
   const newIncome = { ...req.body };
-  console.log(newIncome)
+  console.log(newIncome);
   try {
     await IncomeModel.create(newIncome);
     res.redirect("/incomes"); //redirecting to the list of incomes only

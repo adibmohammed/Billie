e52661644b7ba928f382a90956e184776fed036e
Anna Lefour <anna.lefour@laposte.net>
2021-03-08 12:33:39 +0100

    Add hbs partials and allincomes and allexpenses in app js + incomes folder in views

--- app.js
+++ app.js
@@ -1,76 +1,80 @@
-require("./config/mongodb")
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("./config/mongodb");
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const hbs = require("hbs");
 
 var helpers = require("./helpers/hbs");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
+app.use(express.static(path.join(__dirname, "public")));
+hbs.registerPartials(path.join(__dirname, "views/partials"));
 
 ///ROUTES
-const indexRouter = require('./routes/index');
+//index
+const indexRouter = require("./routes/index");
 //User
-// const createUserRouter = require('./routes/UsersCRUD/createUser')
-// const deleteUserRouter = require('./routes/UsersCRUD/deleteUsers')
-// const readOneUserRouter = require('./routes/UsersCRUD/readOneUser')
-// const updateUserRouter = require('./routes/UsersCRUD/updateUser')
+const createUserRouter = require("./routes/UsersCRUD/createUser");
+const deleteUserRouter = require("./routes/UsersCRUD/deleteUsers");
+const readOneUserRouter = require("./routes/UsersCRUD/readOneUser");
+const updateUserRouter = require("./routes/UsersCRUD/updateUser");
 // //Income
-// const createIncomeRouter = require('./routes/IncomesCRUD/createIncome')
-// const deleteIncomeRouter = require('./routes/IncomesCRUD/deleteIncome')
-// const readOneIncomeRouter = require('./routes/IncomesCRUD/readOneIncome')
-// const updateIncomeRouter = require('./routes/IncomesCRUD/updateIncome')
+const allIncomesRouter = require(".routes/ExpensesCRUD/allIncomes.js");
+const createIncomeRouter = require("./routes/IncomesCRUD/createIncome");
+const deleteIncomeRouter = require("./routes/IncomesCRUD/deleteIncome");
+const readOneIncomeRouter = require("./routes/IncomesCRUD/readOneIncome");
+const updateIncomeRouter = require("./routes/IncomesCRUD/updateIncome");
 // //Expense
-// const createExpenseRouter = require('./routes/ExpensesCRUD/createExpense')
-// const deleteExpenseRouter = require('./routes/ExpensesCRUD/deleteExpense')
-// const readOneExpenseRouter = require('./routes/ExpensesCRUD/readOneExpense')
-// const updateExpenseRouter = require('./routes/ExpensesCRUD/updateExpense')
+const allExpensesRouter = require(".routes/ExpensesCRUD/allExpenses.js");
+const createExpenseRouter = require("./routes/ExpensesCRUD/createExpense");
+const deleteExpenseRouter = require("./routes/ExpensesCRUD/deleteExpense");
+const readOneExpenseRouter = require("./routes/ExpensesCRUD/readOneExpense");
+const updateExpenseRouter = require("./routes/ExpensesCRUD/updateExpense");
 
-
-
-app.use('/', indexRouter);
+//index
+app.use("/", indexRouter);
 // //User
-// app.use('/', createUserRouter)
-// app.use('/', deleteUserRouter)
-// app.use('/', readOneUserRouter)
-// app.use('/', updateUserRouter)
+app.use("/", createUserRouter);
+app.use("/", deleteUserRouter);
+app.use("/", readOneUserRouter);
+app.use("/", updateUserRouter);
 // //Income
-// app.use('/', createIncomeRouter)
-// app.use('/', deleteIncomeRouter)
-// app.use('/', readOneIncomeRouter)
-// app.use('/', updateIncomeRouter)
+app.use("/", allIncomesRouter);
+app.use("/", createIncomeRouter);
+app.use("/", deleteIncomeRouter);
+app.use("/", readOneIncomeRouter);
+app.use("/", updateIncomeRouter);
 // //Expense
-// app.use('/', createExpenseRouter)
-// app.use('/', deleteExpenseRouter)
-// app.use('/', readOneExpenseRouter)
-// app.use('/', updateExpenseRouter)
+app.use("/", allExpensesRouter);
+app.use("/", createExpenseRouter);
+app.use("/", deleteExpenseRouter);
+app.use("/", readOneExpenseRouter);
+app.use("/", updateExpenseRouter);
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 module.exports = app;
--- routes/IncomesCRUD/createIncome.js
+++ routes/IncomesCRUD/createIncome.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const { create } = require("../../models/IncomeModel");
 const router = express.Router();
 const IncomeModel = require("../../models/IncomeModel");
 
 
+//GET route to enter a new income
+router.get("/income/new", (req, res, next) => {
+  res.render("incomes/createIncome");
+});
 
 
-
-
+//POST route to send the new income via a form
+router.post("/income/new", async (req, res, next) => {
+  const newIncome = { ...req.body };
+  console.log(newIncome)
+  try {
+    await IncomeModel.create(newIncome);
+    res.redirect("/incomes"); //redirecting to the list of incomes only
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
 
 module.exports = router;

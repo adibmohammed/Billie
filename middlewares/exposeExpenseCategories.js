e0d26eb01e40725fd3f62233f3a830eb8f884921
Amalia Zebi <amalia.zebi@b4finance.com>
2021-03-10 23:54:59 +0100

    Added a helper we may need and fixed the display depending if logged in or no for the nav and the categories display depending the user account

--- middlewares/exposeExpenseCategories.js
+++ middlewares/exposeExpenseCategories.js
@@ -1,9 +1,24 @@
 module.exports = function exposeExpenseCategories(req, res, next) {
-    if (req.session.currentUser.profile === "business") {
+    if (req.session.currentUser.profile === "Business") {
       res.locals.isBusiness = true;
       res.locals.isFamily = false;
       res.locals.isStudent = false;
       res.locals.isStandard = false;
-    } 
+    } else if (req.session.currentUser.profile === "Family"){
+      res.locals.isBusiness = false;
+      res.locals.isFamily = true;
+      res.locals.isStudent = false;
+      res.locals.isStandard = false;
+    } else if (req.session.currentUser.profile === "Student"){
+      res.locals.isBusiness = false;
+      res.locals.isFamily = false;
+      res.locals.isStudent = true;
+      res.locals.isStandard = false;
+    } else if (req.session.currentUser.profile === "Standard"){
+      res.locals.isBusiness = false;
+      res.locals.isFamily = false;
+      res.locals.isStudent = false;
+      res.locals.isStandard = true;
+    }
     next();
   };

5abdee144484ea6f9df226fd1c458cea8364a1eb
Anna Lefour <anna.lefour@laposte.net>
2021-03-10 21:00:49 +0100

    Chart ongoing and dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var router = express.Router();
+var Chart = require('chart.js');
 
 router.get("/home", function (req, res, next) {
-  res.render("dashboard", { style: ["modalAddAll.css", "dashboard.css"], js: ["modalAddAll.js"] });
+  res.render("dashboard", {
+    style: ["modalAddAll.css", "dashboard.css"],
+    js: ["modalAddAll.js"],
+  });
 });
 
 module.exports = router;

edfb3c94d6e86c9ac31223b2c57560e44f1ac409
Anna Lefour <anna.lefour@laposte.net>
2021-03-11 11:34:22 +0100

    Delete all ok

--- routes/ExpensesCRUD/deleteExpense.js
+++ routes/ExpensesCRUD/deleteExpense.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = new express.Router();
+const UserModel = require("../../models/UserModel");
 const ExpensesModel = require("./../../models/ExpensesModel");
 const protectRoute = require('./../../middlewares/protectRoute');
 
@@ -7,8 +8,11 @@ const protectRoute = require('./../../middlewares/protectRoute');
 router.get("/expenses/delete/:id", protectRoute, (req, res) => {
   ExpensesModel.findByIdAndDelete(req.params.id)
     .then((dbRes) => {
-      res.redirect("/expenses");
+      return UserModel.findByIdAndUpdate(req.session.currentUser._id, {
+        $pull: { myexpense: dbRes._id },
+      })
     })
+    .then(() => {res.redirect("/expenses")})
     .catch((err) => {
       next(err);
     });
--- routes/IncomesCRUD/deleteIncome.js
+++ routes/IncomesCRUD/deleteIncome.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const IncomeModel = require("../../models/IncomeModel");
-const protectRoute = require('./../../middlewares/protectRoute');
+const UserModel = require("../../models/UserModel");
+const protectRoute = require("./../../middlewares/protectRoute");
 
 //Deleting a single income
 router.get("/incomes/delete/:id", protectRoute, (req, res, next) => {
   IncomeModel.findByIdAndDelete(req.params.id)
-    .then(() => {
-      res.redirect("/incomes");
+    .then((dbRes) => {
+      return UserModel.findByIdAndUpdate(req.session.currentUser._id, {
+        $pull: { myincome: dbRes._id },
+      });
     })
+    .then(() => res.redirect("/incomes"))
     .catch((err) => {
       console.log(err);
       next(err);

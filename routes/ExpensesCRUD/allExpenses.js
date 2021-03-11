deb52ed04d330c183b7774aede4512ccca959090
Anna Lefour <anna.lefour@laposte.net>
2021-03-11 14:27:49 +0100

    List working I think

--- routes/ExpensesCRUD/allExpenses.js
+++ routes/ExpensesCRUD/allExpenses.js
@@ -7,11 +7,12 @@ const ExpensesModel = require("./../../models/ExpensesModel");
 /* GET all expenses list */
 
 router.get("/expenses", protectRoute, (req, res, next) => {
-  UserModel.findById(req.session.currentUser.id)
-    .populate("myexpense")
+  // console.log(req.session.currentUser.id)
+  UserModel.findById(req.session.currentUser).populate('myexpense')
     .then((dbRes) => {
+      console.log(dbRes.myexpense)
       res.render("expenses/allExpenses.hbs", {
-        expenses: dbRes,
+        expenses: dbRes.myexpense,
         style: ["allEntries.css", "modalAddAll.css"],
         js: ["modalAddAll.js"],
       });

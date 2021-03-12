6738931b6bfeee1811716ed4436bdba79e4588b0
Amalia Zebi <amalia.zebi@b4finance.com>
2021-03-12 11:26:05 +0100

    fix

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -6,7 +6,7 @@ const multer = require("multer");
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
+  api_secret: process.env.CLOUDINARY_SECRET
 });
 // cloudinary : SAAS platform : specialized in images hosting (tools : metadata, image analyzing ...)
 const storage = new CloudinaryStorage({
--- config/mongodb.js
+++ config/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/billie', { 
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true
